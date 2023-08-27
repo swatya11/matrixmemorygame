4657dffcc28dd22d8855887d30809f65e46bcafc
swatya11 <143314738+swatya11@users.noreply.github.com>
2023-08-27 13:19:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const emojis = ["🎁", "🎁", "😎", "😎", "😁", "😁", "👌", "👌",
+    "😊", "😊", "😂", "😂", "😍", "😍", "👍", "👍"];
+var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+for (var i = 0; i < emojis.length; i++) {
+    let box = document.createElement('div')
+    box.className = 'item';
+    box.innerHTML = shuf_emojis[i]
+    box.onclick = function () {
+        this.classList.add('boxopen')
+        setTimeout(function(){
+            if (document.querySelectorAll('.boxopen').length > 1) {
+                if (document.querySelectorAll('.boxopen')[0].innerHTML ==
+                    document.querySelectorAll('.boxopen')[1].innerHTML) {
+                    document.querySelectorAll('.boxopen')[0].classList.add('boxMatch')
+                    document.querySelectorAll('.boxopen')[1].classList.add('boxMatch')
+                    document.querySelectorAll('.boxopen')[1].classList.remove('boxopen')
+                    document.querySelectorAll('.boxopen')[0].classList.remove('boxopen')
+                    if (document.querySelectorAll('.boxMatch').length == emojis.length) {
+                        alert('win')
+                    }
+                    else {
+                        document.querySelectorAll('.boxopen')[1].classList.remove('boxopen')
+                        document.querySelectorAll('.boxopen')[0].classList.remove('boxopen')
+                    }
+                }
+            }
+        }, 500)
+        }
+    document.querySelector('.game').appendChild(box);
+}
